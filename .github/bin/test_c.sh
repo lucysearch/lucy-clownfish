d6e96cc0c0f6ddb12bd5edafeb0d79551f59b810
Nick Wellnhofer <wellnhofer@aevum.de>
2026-08-24 12:56:26 +0200

    ci: Test C build on Windows

--- .github/bin/test_c.sh
+++ .github/bin/test_c.sh
@@ -2,27 +2,12 @@
 
 set -e
 
-export CC=clang
-export UBSAN_OPTIONS=print_stacktrace=1
-
-# charmonizer doesn't allow to specify linker flags, so we patch the
-# Makefile manually.
-
 cd compiler/c
-./configure -- \
-    -fsanitize=address,undefined \
-    -fno-sanitize-recover=all \
-    -fno-omit-frame-pointer
-sed -i -e 's/CFC_EXE_LDFLAGS = /&-fsanitize=address,undefined /' Makefile
+./configure
 make
-ASAN_OPTIONS=detect_leaks=0 make test
+make test
 
 cd ../../runtime/c
-./configure -- \
-    -fsanitize=address,undefined \
-    -fno-sanitize=function \
-    -fno-sanitize-recover=all \
-    -fno-omit-frame-pointer
-sed -i -e 's/LDFLAGS = /&-fsanitize=address,undefined /' Makefile
+./configure
 make
 make test
