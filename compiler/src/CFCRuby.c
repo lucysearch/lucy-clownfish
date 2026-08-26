lucysearch/lucy-clownfish

commit f819b673f2711fed81c887b367f1c1bfddb9bc16

merge 0.6-0.7 changes (#19)
cherry pick all 0.6 and 0.7 changes and merge back to master

---------

Co-authored-by: Nick Wellnhofer <wellnhofer@aevum.de>

compiler/src/CFCRuby.c
@@ -207,7 +207,7 @@ S_write_boot_c(CFCRuby *self) {
         "\n"
         "void\n"
         "%s() {\n"
-        "    %sbootstrap_parcel();\n"
+        "    %sbootstrap_parcel(void);\n"
         "\n"
         "    cfish_StackString *alias = CFISH_SSTR_WRAP_UTF8(\"\", 0);\n"
         "}\n"

commit 3c25a17380f13f6365b52100c55da9b1382d8183

CFCRuby: Fix bootstrap call
Broke in commit f819b673f.

compiler/src/CFCRuby.c
@@ -207,7 +207,7 @@ S_write_boot_c(CFCRuby *self) {
         "\n"
         "void\n"
         "%s(void) {\n"
-        "    %sbootstrap_parcel(void);\n"
+        "    %sbootstrap_parcel();\n"
         "\n"
         "    cfish_StackString *alias = CFISH_SSTR_WRAP_UTF8(\"\", 0);\n"
         "}\n"